7b173282fc02bd61aa5dc310f51449e031998fdd
박정기 <pjk3665@naver.com>
2025-09-04 17:23:43 +0900

    [level 2] Title: 가장 큰 수, Time: 215.43 ms, Memory: 41.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Linq;
+
+public class Solution {
+    public string solution(int[] numbers) {
+        string answer = "";
+        
+        string[] arr = new string[numbers.Length];
+        for(int i = 0; i < numbers.Length; i++)
+        {
+            arr[i] = numbers[i].ToString();
+        }
+        
+        Array.Sort(arr, (a, b) => string.Compare(b + a, a + b, StringComparison.Ordinal));
+        
+        if(arr[0] == "0") return "0";
+        
+        answer = string.Concat(arr);         
+        
+        return answer;
+    }
+}
