bde64fe4fc13363c7cd2c1bb7af5d0683435a7cf
박정기 <pjk3665@naver.com>
2025-09-11 21:45:16 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.24 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.cs
@@ -0,0 +1,20 @@
+using System;
+
+class Solution
+{
+    public int solution(int n)
+    {
+        int answer = 0;
+
+        while(n != 0)
+        {
+            if(n % 2 == 1)
+            {
+                answer++;
+            }
+            n = n / 2;
+        }
+
+        return answer;
+    }
+}
