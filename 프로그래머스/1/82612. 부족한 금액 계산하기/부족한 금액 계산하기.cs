e9ebb2d9d6ca538b36023ca00be991b5205a1db8
박정기 <pjk3665@naver.com>
2025-09-17 21:22:09 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.27 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Solution
+{
+    public long solution(int price, int money, int count)
+    {
+        long sum = 0;
+        for(int i = 1; i <= count; i++)
+        {
+            sum += (long)price * i;
+        }
+        
+        
+        return (sum - money) > 0 ? (sum - money) : 0;
+    }
+}
