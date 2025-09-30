32e29ee7d831e1a6407eef840849e93362fa26e6
박정기 <pjk3665@naver.com>
2025-09-30 14:48:41 +0900

    [level 1] Title: 약수의 합, Time: 0.23 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int idx = 1;
+        while(idx <= n)
+        {
+            if(n % idx == 0) answer += idx;
+            idx++;
+        }
+        return answer;
+    }
+}
