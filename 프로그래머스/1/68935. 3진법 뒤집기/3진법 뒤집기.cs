5d95ee8ee70ad7479bc6ae217b66e83e80896111
박정기 <pjk3665@naver.com>
2025-10-03 01:03:34 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.16 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        while(n > 0)
+        {
+            answer = answer * 3 + (n % 3);
+            n /= 3;
+        }
+        return answer;
+    }
+}
