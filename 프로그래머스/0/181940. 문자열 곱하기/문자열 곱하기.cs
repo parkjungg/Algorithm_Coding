8f17d9310c822fc8b6a8ae980de96bdc407cfc65
박정기 <pjk3665@naver.com>
2026-01-02 18:57:08 +0900

    [level 0] Title: 문자열 곱하기, Time: 0.26 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Text;
+
+public class Solution {
+    public string solution(string my_string, int k) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < k; i++)
+        {
+            sb.Append(my_string);
+        }
+        return sb.ToString();
+    }
+}
