0f9f30bb7c7ac5b9d7e5cc3fdd8965baa54940eb
박정기 <pjk3665@naver.com>
2026-01-02 18:06:30 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.19 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[2];
+        
+        foreach(var i in num_list)
+        {
+            if(i % 2 == 0) answer[0]++;
+            else answer[1]++;
+        }
+        return answer;
+    }
+}
