27a68e1bcbc9c47f41ea7487e52a1877677cae9e
박정기 <pjk3665@naver.com>
2026-01-05 17:08:51 +0900

    [level 0] Title: 문자 리스트를 문자열로 변환하기, Time: 0.28 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181941. 문자 리스트를 문자열로 변환하기/문자 리스트를 문자열로 변환하기.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Solution {
+    public string solution(string[] arr) {
+        string answer = "";
+        foreach(var s in arr)
+        {
+            answer += s;
+        }
+        return answer;
+    }
+}
