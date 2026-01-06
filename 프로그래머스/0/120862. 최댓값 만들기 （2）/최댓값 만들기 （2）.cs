f35eb00cc8e927202e579263266daeede3f6db1e
박정기 <pjk3665@naver.com>
2026-01-06 16:19:36 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 2.16 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.cs
@@ -0,0 +1,8 @@
+using System;
+
+public class Solution {
+    public int solution(int[] numbers) {
+        Array.Sort(numbers);
+        return numbers[0] * numbers[1] > numbers[numbers.Length - 1] * numbers[numbers.Length - 2] ? numbers[0] * numbers[1] : numbers[numbers.Length - 1] * numbers[numbers.Length - 2];
+    }
+}
