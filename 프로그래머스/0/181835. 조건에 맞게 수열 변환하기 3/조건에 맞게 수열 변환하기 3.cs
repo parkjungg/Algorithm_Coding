b4594e60d4805f28a4dfa24e4fd0c977533a90c6
박정기 <pjk3665@naver.com>
2026-01-07 18:41:08 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 3, Time: 1.89 ms, Memory: 84.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.cs
@@ -0,0 +1,21 @@
+using System;
+
+public class Solution {
+    public int[] solution(int[] arr, int k) {        
+        if(k % 2 == 0)
+        {
+            for(int i = 0; i < arr.Length; i++)
+            {
+                arr[i] += k;
+            }
+        }
+        else
+        {
+            for(int i = 0; i < arr.Length; i++)
+            {
+                arr[i] *= k;
+            }
+        }
+        return arr;
+    }
+}
