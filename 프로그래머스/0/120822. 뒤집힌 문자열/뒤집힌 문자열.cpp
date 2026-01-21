67292c2fab04e9803e960c06dfeb7830b3a3e8c9
박정기 <pjk3665@naver.com>
2026-01-21 20:26:41 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.cpp
@@ -0,0 +1,9 @@
+#include <string>
+#include <vector>
+#include <bits/stdc++.h>
+using namespace std;
+
+string solution(string my_string) {
+    reverse(my_string.begin(), my_string.end());
+    return my_string;
+}
