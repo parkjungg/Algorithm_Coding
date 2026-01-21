16fe53b958481650d918f05a8770e4b1023bcb9c
박정기 <pjk3665@naver.com>
2026-01-21 21:01:44 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <vector>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(string str1, string str2) {
+    if(str1.find(str2) != string::npos) return 1;
+    else return 2;
+}
