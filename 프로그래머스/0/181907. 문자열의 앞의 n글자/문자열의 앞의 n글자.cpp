5a6efca6f2f7eab27c01693e267f41fe5b0184b5
박정기 <pjk3665@naver.com>
2026-01-30 14:16:41 +0900

    [level 0] Title: 문자열의 앞의 n글자, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181907. 문자열의 앞의 n글자/문자열의 앞의 n글자.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string solution(string my_string, int n) {
+    string answer = "";
+    answer = my_string.substr(0, n);
+    return answer;
+}
