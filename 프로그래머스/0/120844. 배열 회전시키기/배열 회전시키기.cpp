96aaa3e53f1641e2a309786df11e5d0d5f604532
박정기 <pjk3665@naver.com>
2026-02-01 18:22:14 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.01 ms, Memory: 4.27 MB -BaekjoonHub

---
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> solution(vector<int> numbers, string direction) {
+    if(direction == "right")
+    {
+        rotate(numbers.rbegin(), numbers.rbegin() + 1, numbers.rend());
+    }
+    else rotate(numbers.begin(), numbers.begin() + 1, numbers.end());
+    return numbers;
+}
