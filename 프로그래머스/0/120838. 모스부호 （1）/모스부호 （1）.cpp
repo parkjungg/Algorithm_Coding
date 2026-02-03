41a424e8f363a2de738499a949fd4cbeb821c54f
박정기 <pjk3665@naver.com>
2026-02-03 15:43:40 +0900

    [level 0] Title: 모스부호 (1), Time: 0.05 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string solution(string letter) {
+    map<string, char> morse = {
+        {".-", 'a'}, {"-...", 'b'}, {"-.-.", 'c'}, {"-..", 'd'}, {".", 'e'},
+        {"..-.", 'f'}, {"--.", 'g'}, {"....", 'h'}, {"..", 'i'}, {".---", 'j'},
+        {"-.-", 'k'}, {".-..", 'l'}, {"--", 'm'}, {"-.", 'n'}, {"---", 'o'},
+        {".--.", 'p'}, {"--.-", 'q'}, {".-.", 'r'}, {"...", 's'}, {"-", 't'},
+        {"..-", 'u'}, {"...-", 'v'}, {".--", 'w'}, {"-..-", 'x'},
+        {"-.--", 'y'}, {"--..", 'z'}
+    };
+    
+    string answer = "";
+    stringstream ss(letter);
+    string word;
+    while(ss >> word)
+    {
+        answer += morse[word];
+    }
+    return answer;
+}
