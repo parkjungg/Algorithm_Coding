946690793e6e4f94e5dfa03be2426e4022981f5a
박정기 <pjk3665@naver.com>
2026-02-11 20:01:28 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.03 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(string my_string) {
+    vector<int> answer(52, 0);
+    for(char c : my_string)
+    {
+        if(c >= 'A' && c <= 'Z')
+        {
+            answer[c - 'A']++;
+        }
+        else
+        {
+            answer[c - 'a' + 26]++;
+        }
+    }
+    return answer;
+}
