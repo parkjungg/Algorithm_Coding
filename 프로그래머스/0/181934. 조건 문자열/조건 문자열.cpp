7d17c2d004fb82a3121376a27e0d8c4cd0eec892
박정기 <pjk3665@naver.com>
2026-02-13 15:14:25 +0900

    [level 0] Title: 조건 문자열, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string ineq, string eq, int n, int m) {
+    if(ineq == "<" && eq == "=") return n <= m ? 1 : 0;
+    else if(ineq == "<" && eq == "!") return n < m ? 1: 0;
+    else if (ineq == ">" && eq == "=") return n >= m ? 1 : 0;
+    else return n > m ? 1 : 0;
+}
