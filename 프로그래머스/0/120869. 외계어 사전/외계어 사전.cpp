44f0f3811e1f3ff0fc112ff7833ffec60b9511db
박정기 <pjk3665@naver.com>
2026-02-18 16:27:14 +0900

    [level 0] Title: 외계어 사전, Time: 0.01 ms, Memory: 3.67 MB -BaekjoonHub

---
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<string> spell, vector<string> dic) {
+    for(string word : dic)
+    {
+        if(word.size() != spell.size())
+            continue;
+        
+        bool isValid = true;
+        
+        for(string s : spell)
+        {
+            if(word.find(s) == string::npos)
+            {
+                isValid = false;
+                break;
+            }
+        }
+        if(isValid)
+            return 1;
+    }
+    return 2;
+}
