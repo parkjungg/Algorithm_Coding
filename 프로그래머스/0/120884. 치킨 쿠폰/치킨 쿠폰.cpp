d9fc112cc07df676872520b734f4f3ce295871be
박정기 <pjk3665@naver.com>
2026-02-18 17:12:19 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int chicken) {
+    int answer = 0;
+    int coupon = chicken;
+    
+    while(coupon >= 10)
+    {
+        int service = coupon / 10;
+        answer += service;
+        
+        coupon = coupon % 10 + service;
+    }
+    return answer;
+}
