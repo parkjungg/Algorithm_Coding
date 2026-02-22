a12eaea06db9005d6c97be706aa1b5792df638dd
박정기 <pjk3665@naver.com>
2026-02-22 17:43:33 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.01 ms, Memory: 4.11 MB -BaekjoonHub

---
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> common) {
+    int answer = 0;
+    if(common[1] - common[0] != common[2] - common[1])
+        answer = common[common.size() - 1] * (common[1] / common[0]);
+    else
+        answer = common[common.size() - 1] + (common[1] - common[0]);
+    return answer;
+}
