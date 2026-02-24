486075cdf1647c87eb31a3dffa5afc0b27a3f90a
박정기 <pjk3665@naver.com>
2026-02-24 18:08:58 +0900

    [level 0] Title: 옹알이 (1), Time: 0.03 ms, Memory: 4.45 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.cpp
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.cpp
@@ -3,50 +3,32 @@
 
 using namespace std;
 
-// int solution(vector<string> babbling) {
-//     int answer = 0;
-//     vector<string> words = {"aya", "ye", "woo", "ma"};
-    
-//     for(int i = 0; i < babbling.size(); i++)
-//     {
-//         for(int j = 0; j < 4; j++)
-//         {
-//             if(babbling[i].find(words[j]) != string::npos)
-//             {
-//                 babbling.replace(babbling[i].find(words[j]), words[j].size(), "0");
-//             }
-//         }
-//     }
-    
-//     for(string s : babbling)
-//     {
-//         for(char c : s)
-//         {
-//             if(c != )
-//             {
-//                 break;
-//             }
-//         }
-//     }
-//     return answer;
- int solution(vector<string> babbling) {
+int solution(vector<string> babbling) {
     int answer = babbling.size();
-    vector<string> V={"aya", "ye", "woo", "ma"};
+    vector<string> words = {"aya", "ye", "woo", "ma"};
     
-    for(int i=0;i<babbling.size();i++){
-        for(int j=0;j<4;j++){       
-            if(babbling[i].find(V[j]) != string::npos)
-                babbling[i].replace(babbling[i].find(V[j]), V[j].size(), "0");
+    for(int i = 0; i < babbling.size(); i++)
+    {
+        for(int j = 0; j < 4; j++)
+        {
+            if(babbling[i].find(words[j]) != string::npos)
+            {
+                babbling[i].replace(babbling[i].find(words[j]), words[j].size(), "0");
+            }
         }
     }
-    for(string s:babbling){
-       for(char c:s){
-           if(c!='0'){
-               answer--;
-               break;
-           }
-       }
-    }
     
+    for(string s : babbling)
+    {
+        for(char c : s)
+        {
+            if(c != '0')
+            {
+                answer--;
+                break;
+            }
+        }
+    }
     return answer;
 }
+ 
