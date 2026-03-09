5188fde663f225da83b631849d6d02f41a32aa4b
박정기 <pjk3665@naver.com>
2026-03-09 22:03:05 +0900

    [Silver V] Title: 영화감독 숌, Time: 60 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+int main()
+{
+    cin >> n;
+    
+    int i = 666;
+    while(true)
+    {
+        if(to_string(i).find("666") != string::npos)
+            n--;
+        if(n == 0)
+            break;
+        i++;
+    }
+    cout << i;
+    return 0;
+}
