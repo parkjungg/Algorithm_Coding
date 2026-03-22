bf9a1e23b6aaa7565c4a1f2f198666152091c5fe
박정기 <pjk3665@naver.com>
2026-03-22 20:33:42 +0900

    [level 1] Title: 모의고사, Time: 0.06 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.cpp
@@ -0,0 +1,39 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> answers) {
+    vector<int> answer;
+    int mx = -1000000;
+    int a[5] = {1, 2, 3, 4, 5};
+    int b[8] = {2, 1, 2, 3, 2, 4, 2, 5};
+    int c[10] = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+    
+    int cnt1 = 0;
+    int cnt2 = 0;
+    int cnt3 = 0;
+    
+    for(int i = 0; i < answers.size(); i++)
+    {
+        if(answers[i] == a[(i % 5)]) cnt1++;
+    }
+    for(int i = 0; i < answers.size(); i++)
+    {
+        if(answers[i] == b[(i % 8)]) cnt2++;
+    }
+    for(int i = 0; i < answers.size(); i++)
+    {
+        if(answers[i] == c[(i % 10)]) cnt3++;
+    }
+    mx = max(max(cnt3, cnt2), cnt1);
+    
+    if(cnt1 == mx) answer.push_back(1);
+    if(cnt2 == mx) answer.push_back(2);
+    if(cnt3 == mx) answer.push_back(3);
+
+    return answer;
+}
+// 1번 : 1 2 3 4 5
+// 2번 : 2 1 2 3 2 4 2 5 
+// 3번 : 3 3 1 1 2 2 4 4 5 5
