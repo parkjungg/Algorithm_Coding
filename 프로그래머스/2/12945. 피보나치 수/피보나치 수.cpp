a851ee5e897cd549ed22b597040a7ebd1e09a3ae
박정기 <pjk3665@naver.com>
2026-03-25 14:02:10 +0900

    [level 2] Title: 피보나치 수, Time: 0.56 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    vector<int> dp(n + 1);
+    
+    dp[0] = 0;
+    if(n >= 1) dp[1] = 1;
+    
+    for(int i = 2; i <= n; i++)
+    {
+        dp[i] = (dp[i - 2] + dp[i - 1]) % 1234567;
+    }
+    return dp[n];
+}
