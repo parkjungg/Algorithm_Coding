f23b86b19a424abedf6f1a4bfce266668a217f40
박정기 <pjk3665@naver.com>
2026-04-01 14:13:02 +0900

    [level 2] Title: 땅따먹기, Time: 6.63 ms, Memory: 25.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
+using namespace std;
+
+int solution(vector<vector<int> > land)
+{
+    int n = land.size();
+    
+    for(int i = 1; i < n; i++)
+    {
+        land[i][0] += max({land[i - 1][1], land[i - 1][2], land[i - 1][3]});
+        land[i][1] += max({land[i - 1][0], land[i - 1][2], land[i - 1][3]});
+        land[i][2] += max({land[i - 1][0], land[i - 1][1], land[i - 1][3]});
+        land[i][3] += max({land[i - 1][0], land[i - 1][1], land[i - 1][2]});
+    }
+
+    return max({land[n - 1][0], land[n - 1][1], land[n - 1][2], land[n - 1][3]});
+}
