32375431ea5ac64da9e8da8139e2431f9aadf9c6
박정기 <pjk3665@naver.com>
2026-05-04 14:58:32 +0900

    [level 2] Title: 디펜스 게임, Time: 24.28 ms, Memory: 40.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(int n, int k, vector<int> enemy) {
+    priority_queue<int> pq;
+    
+    for(int i = 0; i < enemy.size(); i++)
+    {
+        pq.push(enemy[i]);
+        n -= enemy[i];
+        
+        if(n < 0)
+        {
+            if(k > 0)
+            {
+                n += pq.top();
+                pq.pop();
+                k--;
+            }
+            else
+            {
+                return i;
+            }
+        }
+    }
+    return enemy.size();
+}
